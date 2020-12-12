912091b644d662194d65acc8c84dff29ed76aeb4
muhammad ali <ali.mnaeem381@gmail.com>
2020-12-12 20:16:04 +0500

    added 1 new dish component

---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dinner() {
+  return (
+    <div>
+      <h1>hello i am there</h1>
+      <h2>the second thing is that i lve briyani</h2>
+    </div>
+  );
+}
+
+export default Dinner;
